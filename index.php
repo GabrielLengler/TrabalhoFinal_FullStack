62b1e12a80b3050a99577d89b643f7ac0867dce3
GabrielLengler <gabriel.lengler@hotmail.com>
2026-06-25 22:06:40 -0300

    iniciando Home Page, Header e Footer

--- index.php
+++ index.php
@@ -4,8 +4,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WMRPG</title>
+
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
+
+<?php include 'includes/header.php'; ?>
+
+
+<main>
+
+
+
+</main>
+
+
+<?php include 'includes/footer.php'; ?>
     
 </body>
 </html>
