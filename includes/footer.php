16e0d89538a48e6a8efa2c6bd2fd1f8a96978868
GabrielLengler <gabriel.lengler@hotmail.com>
2026-07-07 11:42:47 -0300

    Header e Footer finalizados

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,11 @@
 <footer>
+
     <div class="footer-container">
 
+        <p>
+            © 2026 WorldMarketRPG - Sua campanha começa aqui.
+        </p>
+
     </div>
+
 </footer>
--- includes/header.php
+++ includes/header.php
@@ -2,17 +2,21 @@
 
     <div class="header-container">
 
-    <div class="logo-area">
-        <img src="assets/images/wm-logo.png" alt="Logo WorldMarketRPG" class="logo">
-        <h1>WorldMarketRPG</h1>
-    </div>
+        <a href="index.php" class="logo-link">
+            <div class="logo-area">
+                <img src="assets/images/wm-logo.png" alt="Logo" class="logo">
+                <h1>WorldMarketRPG</h1>
+            </div>
+        </a>
 
         <nav class="navbar">
-
+            <a href="index.php">Início</a>
+            <a href="#">Mundos</a>
+            <a href="#">Criação</a>
         </nav>
 
         <div class="user-area">
-
+            <a href="#" class="login-btn">Entrar / Cadastrar</a>
         </div>
 
     </div>
