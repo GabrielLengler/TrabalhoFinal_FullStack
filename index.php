e5465b7aaae8e168b9b51d7fee8212643c6a137c
GabrielLengler <gabriel.lengler@hotmail.com>
2026-07-08 12:58:25 -0300

    Criado login.php, cadastro.php e config.php

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
+<?php
+require_once 'config.php';
+?>
+
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WMRPG</title>
-    <link rel="icon" type="image/png" href="assets/images/wm-logo.png">
+    <link rel="icon" type="image/png" href="<?= BASE_URL ?>assets/images/wm-logo.png">
 
-    <link rel="stylesheet" href="assets/css/variable.css">
-    <link rel="stylesheet" href="assets/css/global.css">
-    <link rel="stylesheet" href="assets/css/header.css">
-    <link rel="stylesheet" href="assets/css/footer.css">
-    <link rel="stylesheet" href="assets/css/home.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/variable.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/global.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/header.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/footer.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/home.css">
 </head>
 <body>
 
---
+++ pages/login.php
@@ -0,0 +1,31 @@
+<?php
+require_once '../config.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WMRPG</title>
+    <link rel="icon" type="image/png" href="<?= BASE_URL ?>assets/images/wm-logo.png">
+
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/variable.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/global.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/header.css">
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/footer.css">
+</head>
+<body>
+
+<?php include '../includes/header.php'; ?>
+
+<main>
+
+
+    
+</main>
+
+<?php include '../includes/footer.php'; ?>
+    
+</body>
+</html>
