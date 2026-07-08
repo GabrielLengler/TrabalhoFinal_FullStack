c958d3ead8874f1149e6cd9fe02dc3648cc5189c
GabrielLengler <gabriel.lengler@hotmail.com>
2026-07-07 23:15:57 -0300

    Home finalizada (Por enquanto)

--- index.php
+++ index.php
@@ -71,6 +71,59 @@
 
     <div class="section-divider"></div>
 
+    <!-- ===========================
+     ÚLTIMAS ATUALIZAÇÕES
+    =========================== -->
+
+    <section class="home-section latest-updates">
+
+        <div class="home-container">
+
+            <h2 class="section-title">
+                Últimas Atualizações
+            </h2>
+
+        </div>
+
+    </section>
+
+    <div class="section-divider"></div>
+
+    <!-- ===========================
+     APOIE O PROJETO
+    =========================== -->
+
+    <section class="home-section support-project">
+
+        <div class="home-container">
+
+            <h2 class="section-title">
+                Apoie o Projeto ❤️
+            </h2>
+
+            <p class="support-text">
+             O WorldMarketRPG é um projeto independente desenvolvido com muito carinho para facilitar a vida de mestres e jogadores de RPG.
+             <br><br>
+             Toda contribuição será utilizada para manter o site funcionando, custear hospedagem, banco de dados e futuras ferramentas que tornarão a plataforma ainda melhor.
+             <br><br>
+                Além da ajuda financeira, sua doação também demonstra que você acredita na ideia e deseja ver este projeto crescer cada vez mais.
+             Muito obrigado pelo apoio!
+            </p>
+
+            <div class="pix-area">
+
+                <img src="assets/images/qrcode.png" alt="QR Code PIX" class="pix-qrcode">
+
+                <button class="btn-secondary pix-btn" id="copyPix">
+                    Copiar chave PIX
+                </button>
+
+            </div>
+
+        </div>
+
+    </section>
+
 </main>
 
 
